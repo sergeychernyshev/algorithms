9175a13b25466bf9265cf68e651e7d89b0613030
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2022-01-12 18:36:07 -0500

    Factorial recursion is done

--- factorial.js
+++ factorial.js
@@ -1,4 +1,4 @@
-function factorial(input) {
+function factorialIterative(input) {
   let output = 1;
 
   for (let i = 1; i <= input; i++) {
@@ -8,4 +8,12 @@ function factorial(input) {
   return output;
 }
 
-console.log(factorial(10));
+function factorialRecursive(input) {
+  if (input == 1) {
+    return 1;
+  }
+
+  return input * factorialRecursive(input - 1);
+}
+
+console.log(factorialRecursive(3));
